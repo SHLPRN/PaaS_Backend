0097f4751b592d65af79a502e9732e5e9751efd7
SHLPRN <20373672@buaa.edu.cn>
2023-09-11 16:56:01 +0800

    初始化三个APP: application, container, image.

--- PaaS/PaaS/urls.py
+++ PaaS/PaaS/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/application/', include('application.urls')),
+    path('api/container/', include('container.urls')),
+    path('api/image/', include('image.urls')),
 ]
