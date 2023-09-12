976eff9bcac3d2b47e3a5bd72147ed61b349c5b4
OldGreenIsMe <1176606531@qq.com>
2023-09-12 11:36:40 +0800

    feat: 镜像部分基础功能完毕

--- PaaS/image/urls.py
+++ PaaS/image/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('list_images', list_images),
+    path('remove_image', remove_image),
+    path('pull_image', pull_image),
+    path('build_image', build_image),
 ]
--- PaaS/image/views.py
+++ PaaS/image/views.py
@@ -1,1 +1,48 @@
-from django.shortcuts import render
+import docker
+from django.views.decorators.csrf import csrf_exempt
+from kubernetes import client, config
+import yaml
+from django.http import JsonResponse, request
+
+config.load_kube_config()
+
+docker_client = docker.from_env()
+
+@csrf_exempt
+def list_images():
+    images = docker_client.images.list()
+    imageList = []
+    for image in images:
+        dic = {}
+        dic["attrs"] = image.attrs
+        dic["labels"] = image.id
+        dic["labels"] = image.labels
+        dic["short_id"] = image.short_id
+        dic["tags"] = image.tags
+        imageList.append(dic)
+    return JsonResponse({"errno": 0, "imageList": imageList})
+
+
+@csrf_exempt
+def remove_image():
+    image_id = request.POST.get("image_id")
+    docker_client.images.remove(image_id)
+    return JsonResponse({"errno": 0, "msg": "删除成功"})
+
+
+@csrf_exempt
+def pull_image():
+    repository = request.POST.get("repository")
+    try:
+        docker_client.images.pull(repository=repository)
+    except Exception as e:
+        return JsonResponse({"errno": 1, "msg": "没有此镜像"})
+    return JsonResponse({"errno": 0, "msg": "拉取镜像成功"})
+
+
+@csrf_exempt
+def build_image():
+    dockerfile = request.FILES["dockerfile"]
+    tag = request.POST.get("tag")
+    docker_client.images.build(fileobj=dockerfile, tag=tag)
+    return JsonResponse({"errno": 0, "msg": "创建镜像成功"})
