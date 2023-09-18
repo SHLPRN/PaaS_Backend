173c0245223623efebf5cf9b0634d9a26c38ebaf
SHLPRN <20373672@buaa.edu.cn>
2023-09-18 11:31:47 +0800

    fix: 测试run_container接口.

--- PaaS/container/views.py
+++ PaaS/container/views.py
@@ -76,9 +76,11 @@ def run_container(request):
     environment = None if request.POST.get('environment') == '' else request.POST.get('environment')
     volumes = None if request.POST.get('volumes') == '' else request.POST.get('volumes')
     container_ports = request.POST.get('container_ports').split(',')
-    host_posts = request.POST.get('host_ports').split(',')
+    host_ports = request.POST.get('host_ports').split(',')
+    print(request.POST.get('container_ports'), request.POST.get('host_ports'))
+    print(container_ports, host_ports)
     ports = {}
-    for container_port, host_port in zip(container_ports, host_posts):
+    for container_port, host_port in zip(container_ports, host_ports):
         ports[container_port] = host_port
     try:
         container = client.containers.run(image=request.POST.get('image'), name=request.POST.get('name'), ports=ports,
